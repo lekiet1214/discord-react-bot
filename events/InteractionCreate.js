de38c9b04c1f45749633ee597fcf299bdc9c87e9
lekiet1214 <50958432+lekiet1214@users.noreply.github.com>
2024-02-19 19:51:25 +0700

    slashcommand

---
+++ events/InteractionCreate.js
@@ -0,0 +1,23 @@
+const { Collection, Events, Client } = require('discord.js');
+
+module.exports = {
+    name: Events.InteractionCreate,
+    once: false,
+    async execute(interaction) {
+        const command = interaction.client.commands.get(interaction.commandName);
+
+        if (!command) return;
+
+        try {
+            await command.execute(interaction);
+        }
+        catch (error) {
+            console.error(error);
+            if (interaction.replied || interaction.deferred) {
+                await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
+            } else {
+                await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+            }
+        }
+    }
+};
