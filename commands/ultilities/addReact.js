60a4c4976a9278eeb2474cad11c09b6afc6ca8de
lekiet1214 <50958432+lekiet1214@users.noreply.github.com>
2024-02-20 19:11:20 +0700

    reactEmo commands

---
+++ commands/ultilities/addReact.js
@@ -0,0 +1,36 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const fs = require('fs');
+const path = require('path');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('addreact')
+        .setDescription('Add a reaction to the list of reactions')
+        .addStringOption(option =>
+            option.setName('reaction')
+                .setDescription('The reaction to add')
+                .setRequired(true)),
+    async execute(interaction) {
+        // Get emoji from command
+        const emoji = interaction.options.getString('reaction');
+        // Get the path to the file 
+        const filePath = path.join(__dirname, '/../../data/reactData.json');
+        // Read the file
+        const fileData = fs.readFileSync(filePath, 'utf8');
+        // Parse the file
+        const jsonData = JSON.parse(fileData);
+        // Get user's current emoji list
+        const userEmojiList = jsonData[interaction.user.id][interaction.guild.id];
+        // Check if the emoji is already in the list
+        if (userEmojiList.includes(emoji)) {
+            await interaction.reply('You already have this reaction in your list.');
+            return;
+        }
+        // Add the emoji to the list
+        userEmojiList.push(emoji);
+        // Write the file
+        fs.writeFileSync(filePath, JSON.stringify(jsonData, null, 2), 'utf8');
+        await interaction.reply('Reaction added to your list.');
+
+    }
+};
---
+++ commands/ultilities/removeReact.js
@@ -0,0 +1,34 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const fs = require('fs');
+const path = require('path');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('removereact')
+        .setDescription('Add a reaction to the list of reactions')
+        .addStringOption(option =>
+            option.setName('reaction')
+                .setDescription('The reaction to add')
+                .setRequired(false)),
+    async execute(interaction) {
+        // Get emoji from command
+        const emoji = interaction.options.getString('reaction');
+        // Get the path to the file 
+        const filePath = path.join(__dirname, '/../../data/reactData.json');
+        // Read the file
+        const fileData = fs.readFileSync(filePath, 'utf8');
+        // Parse the file
+        const jsonData = JSON.parse(fileData);
+        // Get user's current emoji list
+        const userEmojiList = jsonData[interaction.user.id][interaction.guild.id];
+        // Check if the emoji is already in the list
+        if (userEmojiList.includes(emoji)) {
+            // remove the emoji from the list
+            userEmojiList.splice(userEmojiList.indexOf(emoji), 1);
+        }
+        // Write the file
+        fs.writeFileSync(filePath, JSON.stringify(jsonData, null, 2), 'utf8');
+        await interaction.reply('Reaction added to your list.');
+
+    }
+};
