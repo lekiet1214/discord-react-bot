645d31289ce55050579fa2cc73698d6991cd750e
lekiet1214 <50958432+lekiet1214@users.noreply.github.com>
2024-02-23 13:09:52 +0700

    fix logic

--- commands/ultilities/removeReact.js
+++ commands/ultilities/removeReact.js
@@ -22,12 +22,12 @@ module.exports = {
         // Read the file
         const fileData = fs.readFileSync(filePath, 'utf8');
         // Parse the file
-        const jsonData = JSON.parse(fileData);
+        jsonData = JSON.parse(fileData);
         // Get user's current emoji list
         userEmojiList = jsonData[interaction.user.id][interaction.guild.id];
         // Check if the emoji is already in the list
         if (emoji == 'all') {
-            userEmojiList = [];
+            jsonData[interaction.user.id][interaction.guild.id] = [];
             // Write the file
             fs.writeFileSync(filePath, JSON.stringify(jsonData, null, 2), 'utf8');
             // Upload the file to MongoDB
@@ -38,6 +38,7 @@ module.exports = {
             if (userEmojiList.includes(emoji)) {
                 // remove the emoji from the list
                 userEmojiList.splice(userEmojiList.indexOf(emoji), 1);
+                jsonData[interaction.user.id][interaction.guild.id] = userEmojiList;
             }
             else {
                 return await interaction.reply({ content: `You don't have ${emoji} in your list of reactions`, ephemeral: true });
