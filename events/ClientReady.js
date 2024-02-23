fdc49f72df4543713a7a04aeb3d5ef9f8e2bfa9a
lekiet1214 <50958432+lekiet1214@users.noreply.github.com>
2024-02-23 14:57:23 +0700

    simplify

--- events/ClientReady.js
+++ events/ClientReady.js
@@ -3,10 +3,6 @@ const fs = require('fs');
 const path = require('path');
 const { uploadJson } = require('../mongoDb');
 
-if (process.env.userIdList) {
-    userIdList = process.env.userIdList.split(',');
-}
-
 module.exports = {
     name: Events.ClientReady,
     once: true,
@@ -15,16 +11,8 @@ module.exports = {
         const Guilds = readyClient.guilds.cache.map(guild => guild.id);
         // Write guilds to guildid.json
         const guildIdPath = path.join(__dirname, '../data/guildId.json');
-        // create the data directory if it doesn't exist
-        const dataDir = guildIdPath.substring(0, guildIdPath.lastIndexOf('/'));
-        if (!fs.existsSync(dataDir)) {
-            fs.mkdirSync(dataDir, { recursive: true });
-        }
-        // Create guildId.json if it doesn't exist
-        if (!fs.existsSync(guildIdPath)) {
-            fs.writeFileSync(guildIdPath, JSON.stringify([], null, 2));
-        }
         // Write guilds to guildId.json        
+        fs.writeFileSync(guildIdPath, JSON.stringify([], null, 2));
         fs.writeFileSync(guildIdPath, JSON.stringify(Guilds, null, 2));
         // Upload guilds to MongoDB
         uploadJson(guildIdPath, 'guildId');
