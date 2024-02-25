a10641f184c9849b6cdfc16c60e8b49455f36acc
lekiet1214 <50958432+lekiet1214@users.noreply.github.com>
2024-02-25 19:05:25 +0700

    now caculate real ping and websocket ms

--- commands/ultilities/ping.js
+++ commands/ultilities/ping.js
@@ -5,8 +5,9 @@ module.exports = {
         .setName('ping')
         .setDescription('Replies with Pong!'),
     async execute(interaction) {
-        // Caculate bot's ping
-        const ping = Date.now() - interaction.createdTimestamp;
-        await interaction.reply({ content: `Pong! Bot's ping is ${ping}ms`, ephemeral: true});
+        await interaction.deferReply({ ephemeral: true });
+        await interaction.editReply('Pong!');
+        const sent = await interaction.followUp({ content: `Websocket heartbeat: ${interaction.client.ws.ping}ms.`, ephemeral: true });
+        await interaction.followUp({ content: `Roundtrip latency: ${sent.createdTimestamp - interaction.createdTimestamp}ms`, ephemeral: true });
     }
 };
